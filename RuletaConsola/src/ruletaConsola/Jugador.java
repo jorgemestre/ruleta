32525d7c6d45a46bb856ce4e86a0748fd1ba027a
Jorge Mestre <jorgemestre@gmail.com>
2019-01-28 22:19:27 +0100

    Prueba2

---
+++ RuletaConsola/src/ruletaConsola/Jugador.java
@@ -0,0 +1,25 @@
+package ruletaConsola;
+
+public class Jugador {
+	private int ID;
+	private String nom;
+	private double totalCash;
+	public int getID() {
+		return ID;
+	}
+	public void setID(int iD) {
+		ID = iD;
+	}
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public double getTotalCash() {
+		return totalCash;
+	}
+	public void setTotalCash(double totalCash) {
+		this.totalCash = totalCash;
+	}
+}
