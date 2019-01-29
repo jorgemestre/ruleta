2f18bd156482094e07fc467afe57fbb4a7c165a9
Lucas <lucas_arizaga@hotmail.com>
2019-01-29 15:08:38 +0000

    Getters and Setters para la clase Apuesta

--- RuletaConsola/src/ruletaConsola/Apuesta.java
+++ RuletaConsola/src/ruletaConsola/Apuesta.java
@@ -2,9 +2,9 @@ package ruletaConsola;
 
 public class Apuesta {
 	
-	public String Tipo;
-	public String Detalle;
-	public float Cantidad; 
+	private String Tipo;
+	private String Detalle;
+	private float Cantidad; 
 	
 	Apuesta (String tipo, String detalle, float cantidad)
 	{
@@ -13,5 +13,27 @@ public class Apuesta {
 		Cantidad = cantidad; 
 		
 	}
+	public String getTipo() {
+		return Tipo;
+	}
 
+	public void setTipo(String tipo) {
+		Tipo = tipo;
+	}
+
+	public String getDetalle() {
+		return Detalle;
+	}
+
+	public void setDetalle(String detalle) {
+		Detalle = detalle;
+	}
+
+	public float getCantidad() {
+		return Cantidad;
+	}
+
+	public void setCantidad(float cantidad) {
+		Cantidad = cantidad;
+	}
 }
