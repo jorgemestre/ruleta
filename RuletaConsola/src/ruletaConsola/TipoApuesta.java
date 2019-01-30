3f4425f42a7c876d92698d4bd42ed9a006b6e75c
Lucas <lucas_arizaga@hotmail.com>
2019-01-30 09:25:10 +0000

    Clase tipo Apuesta creada, solo se especifican 3 tipos de apuesta (pleno, color y docena). Falta crear el resto.

--- RuletaConsola/src/ruletaConsola/TipoApuesta.java
+++ RuletaConsola/src/ruletaConsola/TipoApuesta.java
@@ -1,5 +1,66 @@
 package ruletaConsola;
 
+import java.util.ArrayList;
+
 public class TipoApuesta {
+	
+	private String Nombre; 
+	private String Params;
+	private int Ganancia;
+	
+	
+	//contructor parametrizado
+	TipoApuesta(String nombre, String params)
+	{
+		this.Nombre = nombre; 
+		this.Params = params; 
+		setGanancia();
+	}
+
+	
+	private void setGanancia()
+	{
+		
+		//Hay que completar con todos los tipos de apuesta, 
+		//por ahora solo unos pocos para probar
+		
+		switch (this.Nombre)
+		{
+		case "pleno": this.Ganancia= 36; 
+			break;
+		case "color": this.Ganancia = 2; 
+			break;
+		case "docena": this.Ganancia = 3;
+			break;
+		default: this.Ganancia = 0 ; 
+		
+		}
+		
+	}
+	
+	
+	
+	public String getNombre() {
+		return Nombre;
+	}
+
+	public void setNombre(String nombre) {
+		Nombre = nombre;
+	}
+
+	public String getParams() {
+		return Params;
+	}
+
+	public void setParams(String params) {
+		this.Params = params;
+	}
+
+	public int getGAnancia() {
+		return Ganancia;
+	}
+	
+	
+	
 
 }
